c84f698e463ea2a8701aa52ca23b013239f8a19a
Preetraj Haldar <preetrajhaldar2002@gmail.com>
2023-04-13 12:09:53 +0530

    initial commit

---
+++ app/contracts/Agent.sol
@@ -0,0 +1,119 @@
+//SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.1;
+
+contract Agent {
+    
+    struct patient {
+        string name;
+        uint age;
+        address[] doctorAccessList;
+        uint[] diagnosis;
+        string record;
+    }
+    
+    struct doctor {
+        string name;
+        uint age;
+        address[] patientAccessList;
+    }
+
+    uint creditPool;
+
+    address[] public patientList;
+    address[] public doctorList;
+
+    mapping (address => patient) patientInfo;
+    mapping (address => doctor) doctorInfo;
+ //   mapping (address => address) Empty;
+    // might not be necessary
+    mapping (address => string) patientRecords;
+    
+
+
+    function add_agent(string memory _name, uint _age, uint _designation, string memory _hash) public returns(string memory){
+        address addr = msg.sender;
+        
+        if(_designation == 0){
+            patient memory p;
+            p.name = _name;
+            p.age = _age;
+            p.record = _hash;
+            patientInfo[msg.sender] = p;
+            patientList.push(addr);
+            return _name;
+        }
+       else if (_designation == 1){
+            doctorInfo[addr].name = _name;
+            doctorInfo[addr].age = _age;
+            doctorList.push(addr);
+            return _name;
+       }
+       else{
+           revert();
+       }
+    }
+
+
+    function get_patient(address addr) view public returns (string memory , uint, uint[] memory , string memory ){
+        // if(keccak256(patientInfo[addr].name) == keccak256(""))revert();
+        return (patientInfo[addr].name, patientInfo[addr].age, patientInfo[addr].diagnosis, patientInfo[addr].record);
+    }
+
+    function get_doctor(address addr) view public returns (string memory , uint){
+        // if(keccak256(doctorInfo[addr].name)==keccak256(""))revert();
+        return (doctorInfo[addr].name, doctorInfo[addr].age);
+    }
+    function get_patient_doctor_name(address paddr, address daddr) view public returns (string memory , string memory ){
+        return (patientInfo[paddr].name,doctorInfo[daddr].name);
+    }
+
+    function permit_access(address addr)  public {
+       // require(msg.value == 500);
+
+      //  creditPool += 500;
+        
+        doctorInfo[addr].patientAccessList.push(msg.sender);
+        patientInfo[msg.sender].doctorAccessList.push(addr);
+        
+    }
+
+  
+
+
+    
+    function get_accessed_doctorlist_for_patient(address addr) public view returns (address[] memory )
+    { 
+        address[] storage doctoraddr = patientInfo[addr].doctorAccessList;
+        return doctoraddr;
+    }
+    function get_accessed_patientlist_for_doctor(address addr) public view returns (address[] memory )
+    {
+        return doctorInfo[addr].patientAccessList;
+    }
+
+    
+   /** function revoke_access(address daddr) public payable{
+        remove_patient(msg.sender,daddr);
+        msg.sender.transfer(2 ether);
+        creditPool -= 2;
+    }
+    **/
+
+    function get_patient_list() public view returns(address[] memory ){
+        return patientList;
+    }
+
+    function get_doctor_list() public view returns(address[] memory ){
+        return doctorList;
+    }
+
+    function get_hash(address paddr) public view returns(string memory ){
+        return patientInfo[paddr].record;
+    }
+
+    function set_hash(address paddr, string memory _hash) internal {
+        patientInfo[paddr].record = _hash;
+    }
+
+}
